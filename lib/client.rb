0b8530260146c32d5712fb9302ed2898507fd5a8
stephen <lonelytreesl@yahoo.com>
2015-05-08 19:47:24 +0000

    add spec and code for .all() to client.rb, all specs passing.

--- lib/client.rb
+++ lib/client.rb
@@ -4,4 +4,14 @@ class Client
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
   end
+  
+  define_singleton_method(:all) do
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each() do |client|
+      name = client.fetch("name") 
+      clients.push(Client.new({:name => name}))
+    end
+    clients
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,4 +7,9 @@ describe(Client) do
       expect(test_client.name).to(eq("Beth"))
     end
   end
+    describe('.all') do
+    it('returns an array of clients, is empty at first') do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
 end
